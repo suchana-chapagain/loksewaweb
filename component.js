cebaa61d01d09f50c8c5e8dc7bad7493ce2cd602
Anuj Acharya <acharyaanuj45@gmail.com>
2024-06-18 12:18:02 +0545

    Components Created

---
+++ component.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#nav-placeholder").load("/components/nav.html");
+});
+
+$(function(){
+    $("#footer-placeholder").load("/components/footer.html");
+})
--- script.js
+++ script.js
@@ -8,3 +8,4 @@ burger.addEventListener("click", () => {
 
 
 
+
