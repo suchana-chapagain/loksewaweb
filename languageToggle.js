2e1518a16fd838ab08cf00473f8ef5a2d60b165d
Anuj Acharya <acharyaanuj45@gmail.com>
2024-07-13 11:16:27 +0545

    Issues Resolving On Progress

--- languageToggle.js
+++ languageToggle.js
@@ -1,8 +1,13 @@
 document.getElementById('language-toggle').addEventListener('change', function () {
   var elements = document.querySelectorAll('[data-en]'); // Select all elements with data-en attribute
+  var inputs = document.querySelectorAll('input[data-en-placeholder]');
   var newLang = this.checked ? 'ne' : 'en';
 
   elements.forEach(function (element) {
       element.textContent = element.getAttribute('data-' + newLang);
   });
+
+  inputs.forEach(function (input) {
+    input.setAttribute('placeholder', input.getAttribute('data-' + newLang + '-placeholder'));
+});
 });
