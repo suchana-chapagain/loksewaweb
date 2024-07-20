e8cb24462bec6bd19e8bc99c3c5fe6b7b29ccbd0
Anuj Acharya <acharyaanuj45@gmail.com>
2024-07-20 20:31:50 +0545

    Model Set Format Completed

---
+++ pages/displayToggle.js
@@ -0,0 +1,5 @@
+const modelDiv = document.querySelector("#modelDiv");
+var model = document.getElementById("modelContent");
+modelDiv.addEventListener('click' , function() {
+  model.style.display = "initial";
+})
