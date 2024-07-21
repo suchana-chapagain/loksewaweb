a3dc682d4eb7d1b460625981cbce0fcb2f1a86ec
Anuj Acharya <acharyaanuj45@gmail.com>
2024-07-21 16:44:34 +0545

    Study Format in Progress

--- Study/contentToggle.js
+++ Study/contentToggle.js
@@ -5,10 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
   const gkSelect1 = document.querySelector("#gkSelect1");
   const gkSelect2 = document.querySelector("#gkSelect2");
   const gkSelect3 = document.querySelector("#gkSelect3");
+  const jobSelect1 = document.querySelector("#gkSelect1");
+  const jobSelect2 = document.querySelector("#gkSelect2");
+  const jobSelect3 = document.querySelector("#gkSelect3");
   const set1 = document.querySelector(".set1");
   const set2 = document.querySelector(".set2");
   const topic1 = document.querySelector(".topic1");
   const topic2 = document.querySelector(".topic2");
+  const job1 = document.querySelector(".job1");
+  const job2 = document.querySelector(".job2");
 
   modelSelect1.addEventListener('click', function() {
     set1.style.display = "block";
@@ -28,5 +33,15 @@ document.addEventListener("DOMContentLoaded", function() {
   gkSelect2.addEventListener('click', function() {
     topic1.style.display = "none";
     topic2.style.display = "block";
+  });
+
+  jobSelect1.addEventListener('click', function() {
+    job1.style.display = "block";
+    job2.style.display = "none";
+  });
+
+  jobSelect2.addEventListener('click', function() {
+    job1.style.display = "none";
+    job2.style.display = "block";
   });
 });
--- Study/displayToggle.js
+++ Study/displayToggle.js
@@ -1,16 +1,28 @@
 var selectMode = document.getElementById("selectMode");
 const modelDiv = document.querySelector("#modelDiv");
 const gkDiv = document.querySelector("#gkDiv");
+const jobDiv = document.querySelector("#jobDiv");
 var model = document.getElementById("modelContent");
 var gk = document.getElementById("gkContent");
+var job = document.getElementById("jobContent");
+
 modelDiv.addEventListener('click' , function() {
+  job.style.display = "none";
   gk.style.display = "none";
   model.style.display = "initial";
   selectMode.style.display = "none";
 })
 
 gkDiv.addEventListener('click' , function() {
+  job.style.display = "none";
   model.style.display = "none";
   gk.style.display = "initial";
+  selectMode.style.display = "none";
+})
+
+jobDiv.addEventListener('click' , function() {
+  model.style.display = "none";
+  gk.style.display = "none";
+  job.style.display = "initial";
   selectMode.style.display = "none";
 })
